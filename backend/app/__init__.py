f0e6c4ea87c90edbc524abda907c9444d363946b
Feng Yilong <fylnushs@gmail.com>
2025-06-17 15:35:14 +0800

    Change init to resolve CORS problem

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,7 +6,8 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # enable CORS for all routes
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://127.0.0.1:5173"]}})
+    #CORS(app)  # enable CORS for all routes
     app_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(app_dir, "SampleData.json")
     app.dl = DataLoader(data_path)
