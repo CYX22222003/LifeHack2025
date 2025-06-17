8ba8bf037e1f2417df2cd3caa588be34798da21d
CYX22222003 <e1155533@u.nus.edu>
2025-06-17 13:33:35 +0800

    Small fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from .routes import api
 from .DataLoader import DataLoader
+import os
 
 def create_app():
     app = Flask(__name__)
     CORS(app)  # enable CORS for all routes
-    app.dl = DataLoader("SampleData.json")
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(app_dir, "SampleData.json")
+    app.dl = DataLoader(data_path)
     app.register_blueprint(api)
 
     return app
--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@ from app.DataLoader import DataLoader
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=5000)
